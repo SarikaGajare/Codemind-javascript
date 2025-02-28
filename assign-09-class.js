68d2f168ba131dbc3771e143fab0f99a706252c8
SarikaGajare <sarikaGajare777@gmail.com>
2025-02-28 16:55:09 +0530

    Update assign-09-class.js

--- assign-09-class.js
+++ assign-09-class.js
@@ -1,3 +1,4 @@
+console.log(`1.stmt===>`);
 class Vehicle{
     // Data members
     // Constructor
@@ -17,3 +18,37 @@ class Vehicle{
 const vehicle1 = new Vehicle("Maruti swift", "4 wheeler", "mh-13", "white");
 vehicle1.detail();
 //console.log(vehicle1);
+const vehicle2 = new Vehicle("Venue", "4 wheeler", "mh-12", "Black");
+vehicle2.detail();
+const vehicle3 = new Vehicle("Activa", "2 wheeler", "mh-11", "red");
+vehicle3.detail();
+const vehicle4 = new Vehicle("Range Rover", "4 wheeler", "mh-14", "greay");
+vehicle4.detail();
+
+console.log(`2 stmt:===>`);
+
+class College {
+  constructor  (college_name,college_address,college_rank,univercity)
+        {
+            this.college_name=college_name;
+            this.college_address=college_address;
+            this.college_rank=college_rank;
+            this.univercity=univercity;
+
+        }
+     display(){
+ console.log (`College-Name:${this.college_name} 
+             College-Address:${this.college_address}
+             College-Rank: ${this.college_rank} 
+             Univercity: ${this.univercity}`)};
+    
+    }
+ const college1=new College("COEP", "pune","1","UniPune");
+ college1.display();
+const college2=new College("PVG","pune","5","UniPune");
+college2.display();
+const college3=new College("Shinhgad","splapur","3","solapur");
+college3.display();
+const college4=new College("fabtek","sangola","7","solapur");
+college4.display();
+
