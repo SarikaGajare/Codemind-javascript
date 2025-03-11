4e654f207e4e8831ecba5d2fd5f78300f0b7c86b
SarikaGajare <sarikaGajare777@gmail.com>
2025-03-11 17:28:30 +0530

    dom Assignment

---
+++ assign-14-main.js
@@ -0,0 +1,5 @@
+const profile_Head =document.getElementById("profileHead");
+profile_Head.style.color="Orange";
+
+const tech_Stack = document.getElementById("techStack")
+tech_Stack.style.color="Red";
